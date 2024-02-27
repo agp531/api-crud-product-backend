24a49efa7f483c766d432878aa38e3e249db9f2c
Allan <allan.g531@hotmail.com>
2024-02-27 04:23:29 -0300

    created new route for upload image

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('products', ProductController::class);
+Route::post('/uploadFile/{product}', [ProductController::class, 'uploadFile']);
